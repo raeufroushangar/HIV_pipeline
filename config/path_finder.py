c1f051fe5e60be02d108e3041a9d76b364c8f9a0
Raeuf Roushangar <raeuf@pn2307058.lanl.gov>
2024-04-24 16:00:15 -0600

    Deleted unused files and added new files to the config directory

--- config/path_finder.py
+++ config/path_finder.py
@@ -1,18 +1,30 @@
 import os
 
-def find_apps_in_external_apps_dir():
-    # Find the current working directory of the script
-    current_directory = os.getcwd()
+def find_apps_in_external_apps_dir(target_dir):
+    """
+    Find the path to the specified target directory within the 'hiv_desktop_app' directory.
 
-    # Search for the 'hiv_desktop_app' directory starting from the current directory
-    while 'hiv_desktop_app' not in os.listdir(current_directory):
-        current_directory = os.path.dirname(current_directory)
+    Parameters:
+    - target_dir (str): The name of the target directory ('external_apps' or 'database').
 
-    # Once the 'hiv_desktop_app' directory is found, construct the path to Muscle 
-    app_path = os.path.join(current_directory, 'hiv_desktop_app/bin/external_apps')
+    Returns:
+    - app_path (str): The path to the target directory if found, otherwise None.
+    """
+    try:
+        # Find the current working directory of the script
+        current_directory = os.getcwd()
 
-    # Check if the Muscle executable exists at the specified path
-    if os.path.exists(app_path):
-        return app_path
-    else:
-        return None
+        # Search for the 'hiv_desktop_app' directory starting from the current directory
+        while 'hiv_desktop_app' not in os.listdir(current_directory):
+            current_directory = os.path.dirname(current_directory)
+
+        # Once the 'hiv_desktop_app' directory is found, construct the path to the target directory
+        app_path = os.path.join(current_directory, f'hiv_desktop_app/bin/{target_dir}')
+
+        # Check if the target directory exists at the specified path
+        if os.path.exists(app_path):
+            return app_path
+        else:
+            return None
+    except Exception as e:
+        return f"Error: {str(e)}"
